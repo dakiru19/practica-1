bee2f41915e08e287531ddc142acfd2320046935
dakiru19 <sharadremurr19@gmail.com>
2026-04-17 23:09:24 -0600

    cree una clase y añadi atributos

---
+++ codigoprueba.py
@@ -0,0 +1,8 @@
+class Actividades:
+    def __init__(self, nombre_tarea, tiempo_estimado, prioridad, completada, responsable):
+        self.__nombre_tarea = nombre_tarea
+        self.__tiempo_estimado = tiempo_estimado  
+        self.__prioridad = prioridad  
+        self.__completada = completada 
+        self.__responsable = responsable
+        
